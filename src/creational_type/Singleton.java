8000232f31ea87672eb3de17b26eda98fbe360e7
nupur <nupur.agrawal@mastercard.com>
2019-05-13 15:47:49 +0530

    commit

---
+++ src/creational_type/Singleton.java
@@ -0,0 +1,13 @@
+package creational_type;
+
+/*
+ * Ensure that only one instance of a class is created.
+ * Provide a global point of access to the object.
+ */
+public class Singleton {
+	public static void main(String[] args) {
+		
+	}
+	
+
+}
